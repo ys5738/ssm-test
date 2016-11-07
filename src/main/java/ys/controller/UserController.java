3acd793c5872bdac701314ef74d153ec9eed3682
ys5738 <573869800@qq.com>
2016-11-07 13:02:03 +0800

    second 改了一些配置

--- src/main/java/ys/controller/UserController.java
+++ src/main/java/ys/controller/UserController.java
@@ -3,7 +3,6 @@ package ys.controller;
 import java.util.List;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
